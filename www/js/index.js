ac69c0a834a6d5fae97fa7b7f089f16735372457
franckChev <franck.chev@hotmail.fr>
2014-09-11 02:48:25 +0200

    -+

--- www/js/index.js
+++ www/js/index.js
@@ -19,17 +19,69 @@
 
 var smartMediBoxApp = angular.module('SmartMediBoxApp', ['ngRoute']);
 
-smartMediBoxApp.config(function ($routeProvider) {
-    $routeProvider.when("/stock", { templateUrl: "/SmartMedibox/www/views/stock.html" }).
-                   when("/planning", { templateUrl: "/SmartMedibox/www/views/planning.html" }).
-                   when("/addDrugs", { templateUrl: "/SmartMedibox/www/views/addDrugs.html" }).
-                   when("/param", { templateUrl: "/SmartMedibox/www/views/param.html" }).
-                   otherwise({ redirectTo: '/' });
+smartMediBoxApp.config(function($routeProvider) {
+    $routeProvider.when("/stock", {
+        templateUrl: "views/stock.html"
+    }).
+    when("/planning", {
+        templateUrl: "views/planning.html"
+    }).
+    when("/addDrugs", {
+        templateUrl: "views/addDrugs.html"
+    }).
+    when("/addTask", {
+        templateUrl: "views/addTask.html"
+    }).
+    when("/param", {
+        templateUrl: "views/param.html"
+    }).
+    otherwise({
+        redirectTo: '/stock'
+    });
 });
 
-function StockCtrl($scope){
+smartMediBoxApp.controller('StockCtrl', function($scope) {
     $scope.medocs = JSON.parse(localStorage.stock);
-}
+
+    $scope.takeDrugs = function(index) {
+        var stock = JSON.parse(localStorage.stock);
+        var sortedKeys = Object.keys(stock).sort();
+        stock[sortedKeys[index]].quantity -= 1;
+        if (stock[sortedKeys[index]].quantity == 0) {
+            delete stock[sortedKeys[index]];
+        }
+        $scope.medocs = stock;
+        localStorage.stock = JSON.stringify(stock);
+        window.location.href = "#stock";
+    };
+});
+
+smartMediBoxApp.controller('TasksCtrl', function($scope) {
+    $scope.medocs = JSON.parse(localStorage.stock);
+    $scope.tasks = JSON.parse(localStorage.planning);
+    $scope.addTask = function() {
+        var planning;
+        if (localStorage.planning != undefined) {
+            planning = JSON.parse(localStorage.planning);
+        } else {
+            planning = new Object();
+        }
+        if ($scope.leMedoc != undefined) {
+            planning[$scope.leMedoc] = {
+                name: $scope.leMedoc
+            }
+        };
+        localStorage.planning = JSON.stringify(planning);
+        window.location.href = "#planning";
+    };
+    $scope.deleteTask = function(index) {
+        var planning = JSON.parse(localStorage.planning);
+        var sortedKeys = Object.keys(planning).sort();
+        delete planning[sortedKeys[index]];
+        $scope.tasks = planning;
+        localStorage.planning = JSON.stringify(planning);
+    }
+});
 
 var app = {
     // Application Constructor
